e671227e5bf65fbed953c1fc2b36ca2e93e08ac7
Vince Davis <vdavis@acculynx.com>
2019-10-07 22:26:21 -0500

    removed committed UIWebView logic

--- Tests/RxCocoaTests/UIWebView+RxTests.swift
+++ Tests/RxCocoaTests/UIWebView+RxTests.swift
@@ -17,51 +17,5 @@ import XCTest
 final class UIWebViewTests: RxTest {}
 
 fileprivate let testHTMLString = "<html><head></head><body><h1>🔥</h1></body></html>"
-/*
-extension UIWebViewTests {
-        
-    func testDidStartLoad() {
-        let webView = UIWebView()
-        var didStartLoad = false
 
-        let subscription = webView.rx.didStartLoad.subscribe(onNext: { _ in
-            didStartLoad = true
-        })
-
-        webView.delegate!.webViewDidStartLoad!(webView)
-
-        XCTAssertTrue(didStartLoad)
-        subscription.dispose()
-    }
-    
-    func testDidFinishLoad() {
-        let webView = UIWebView()
-        var didFinishLoad = false
-
-        let subscription = webView.rx.didFinishLoad.subscribe(onNext: { _ in
-            didFinishLoad = true
-        })
-
-        webView.delegate!.webViewDidFinishLoad!(webView)
-
-        XCTAssertTrue(didFinishLoad)
-        subscription.dispose()
-    }
-
-    func testDidFailLoad() {
-        let webView = UIWebView()
-        var didFailLoad = false
-
-        let subscription = webView.rx.didFailLoad.subscribe { _ in
-            didFailLoad = true
-        }
-
-        webView.delegate!.webView!(webView, didFailLoadWithError: NSError(domain: "", code: 0, userInfo: .none))
-
-        XCTAssertTrue(didFailLoad)
-        subscription.dispose()
-    }
-
-}
-*/
 #endif
